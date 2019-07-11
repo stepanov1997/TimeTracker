9aedd3841b9e22b1c6512a139f4d32d51de45ac5
stepanov1997 <kristijan.stepanov95@gmail.com>
2019-07-11 17:37:50 +0200

    add TimeTrackerDbContext

---
+++ src/TimeTracker/Data/TimeTrackerDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeTracker.Domain;
+
+namespace TimeTracker.Data
+{
+    public class TimeTrackerDbContext : DbContext
+    {
+        public TimeTrackerDbContext(DbContextOptions<TimeTrackerDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TimeEntry> TimeEntries { get; set; }
+    }
+}
