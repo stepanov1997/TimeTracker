5be564a9d82f68934af4f2b08cdf611b076c7b45
stepanov1997 <kristijan.stepanov95@gmail.com>
2019-07-12 19:14:35 +0200

    Add validators for input models

---
+++ src/TimeTracker/Models/Validation/ProjectInputModeValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Models.Validation
+{
+    public class ProjectInputModeValidator : AbstractValidator<ProjectInputModel>
+    {
+        public ProjectInputModeValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .Length(0, 100);
+        }
+    }
+}
--- src/TimeTracker/Models/Validation/UserInputModeValidator.cs
+++ src/TimeTracker/Models/Validation/UserInputModeValidator.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 
 namespace TimeTracker.Models.Validation
 {
-    public class UserInputModeValidator : AbstractValidator<UserModel>
+    public class UserInputModelValidator : AbstractValidator<UserInputModel>
     {
-        public UserInputModeValidator()
+        public UserInputModelValidator()
         {
             RuleFor(x => x.Name)
                 .NotEmpty()
+                .NotNull()
                 .Length(1, 100);
 
             RuleFor(x => x.HourRate)
+                .NotNull()
                 .GreaterThan(0)
                 .LessThan(1000);
         }
